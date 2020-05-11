44154aef8c161ca8accfd411d8a024102ee3ae3d
mokyoungg <mokyoungg@gmail.com>
2020-05-11 21:59:27 +0900

    JS반복문

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-
+/*
 const comment = document.getElementsByClassName('commenting')[0];
 const commentInput = document.getElementsByClassName('commenting_input')[0];
 const commentButton = document.getElementsByClassName('commenting_button')[0];
@@ -14,7 +14,7 @@ comment.addEventListener('keyup', function(){
     }
 });
 
-commentInput.addEventListener('keydown', function(event){
+commentInput.addEventListener('keydown', function(){
     if(event.keyCode === 13){
         let dTag = document.createElement('div');
         dTag.innerHTML = commentInput.value.slice(0, commentInput.value.length);
@@ -30,23 +30,56 @@ commentButton.addEventListener('click', function(){
     }
 })
 
+*/
 
 
-/*
 const comment = document.getElementsByClassName('commenting');
 const commentInput = document.getElementsByClassName('commenting_input');
 const commentButton = document.getElementsByClassName('commenting_button');
 const reply = document.getElementsByClassName('reply');
 
+// 오류 찾기 console.log("sdfsdfsfd: ", commentInput)
 
-comment.addEventListener('keyup', function(){
-    for (let i=0 ; i < comment.length ; i++){
+for (let i=0; i<comment.length; i++){git 
+    commentInput[i].addEventListener('keyup', function(){
+      if(commentInput[i].value.length > 0){
+        commentButton[i].style.color = 'blue';
+        return;
+      }else {
+        commentButton[i].style.color = '#b2dffc';
+        return;
+      }
+    })
+  }
+
+for (let i=0; i < comment.length; i++){
+  commentInput[i].addEventListener('keydown', function(event){
+    if(event.keyCode === 13){
+        let dTag = document.createElement('div');
+        dTag.innerHTML = commentInput[i].value;
+        reply[i].appendChild(dTag);
+        
+    }
+})
+}
+
+for (let i=0; i < comment.length; i++){
+commentButton[i].addEventListener('click', function(){
+    if(commentButton[i].style.color === 'blue'){
+        let dTag = document.createElement('div');
+        dTag.innerHTML = commentInput[i].value;
+        reply[i].appendChild(dTag);
+    }
+})
+}
+  /*
+
+commentInput.addEventListener('keyup', function(){
+    for (let i=0 ; i < commentInput.length ; i++){
         if(commentInput[i].value.length > 0) {
             commentButton[i].style.color = 'blue';
-          
         }else {
             commentButton[i].style.color = '#b2dffc';
-            
         }
     }   
 });
